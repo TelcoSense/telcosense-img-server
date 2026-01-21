3977d1307d67a7a1e275ee76f3f17188542e3145
stepanmk <smiklanek@gmail.com>
2026-01-21 20:42:10 +0100

    add rain sum

--- backend/app_config.py
+++ backend/app_config.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 IMG_DIRS = {
     "raincz": Path("../telcorain/outputs_web").resolve(),
+    "rainsum": Path("../telcorain/outputs_sum").resolve(),
     "tempcz": Path("../telcotemp/outputs_cml").resolve(),
     "tempchmi": Path("../telcotemp/outputs_meteo").resolve(),
 }
